8810d8a6298af3702e6091e4fc50ee96d85a93c6
DESKTOP-L7H1FM4\Usuario <rodrigo_roldan18@hotmail.com>
2023-12-18 12:27:37 +0100

    Primer commit Spring Initializr

---
+++ src/main/java/com/example/proyectoAD/ProyectoAdApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyectoAD/ProyectoAdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoAD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
